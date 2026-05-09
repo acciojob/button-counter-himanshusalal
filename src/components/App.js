b86ee980d7b35059de16005c16c82503b20fde0a
himanshusalal <himanshusalal517@gmail.com>
2026-05-09 14:48:19 +0530

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const  [count, setcount]= useState(0)
   return (
     <div>
         {/* Do not remove the main div */}
+       <h1>{count}</h1>
+   <button onClick={() => setcount(count + 1)}>
+      Increment
+     </button>
     </div>
   )
 }
